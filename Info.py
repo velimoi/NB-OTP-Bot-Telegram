901e23a4822ad4f547363770f243599204392b2e
Gear7926 <ferocious7962@outlook.com>
2025-04-30 23:23:20 +0400

    Info.py

---
+++ Info.py
@@ -0,0 +1,22 @@
+#Twilio
+
+account_sid = ''
+auth_token = ''
+twilionumber = ''
+twiliosmsnumber = ''
+
+#Telegram Bot Token
+API_TOKEN = "" 
+
+#Host URL
+callurl = 'https://03f3-196-191-60-243.ngrok.io' 
+twiliosmsurl = 'https://03f3-196-191-60-243.ngrok.io/sms'
+
+
+
+
+
+
+
+
+
