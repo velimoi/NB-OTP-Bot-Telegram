28e4dd4a7a825f6a8f7d1fd04fd7c33780ebe643
velimoi <30995238+velimoi@users.noreply.github.com>
2025-07-19 04:46:50 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1177,6 +1177,5 @@ def handle_recordings(userid):
 
 
 
-if __name__ == "__main__" app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
-    
-app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True)
