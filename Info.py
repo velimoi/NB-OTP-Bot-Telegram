2f441d5eeedf62e354f3d105e1048b72c198c7f4
velimoi <30995238+velimoi@users.noreply.github.com>
2025-07-19 04:02:38 +0300

    Update Info.py

--- Info.py
+++ Info.py
@@ -9,8 +9,8 @@ twiliosmsnumber = ''
 API_TOKEN = "8078682981:AAECiP6bH9D7N7T6pA0HnBjnuOfBHKyHIT0" 
 
 #Host URL
-callurl = 'https://03f3-196-191-60-243.ngrok.io' 
-twiliosmsurl = 'https://03f3-196-191-60-243.ngrok.io/sms'
+callurl = '' 
+twiliosmsurl = ''
 
 
 
