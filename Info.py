80c5f00ea148d71bd4317f5c892f8e5e70614bdb
velimoi <30995238+velimoi@users.noreply.github.com>
2025-07-19 03:49:28 +0300

    Update Info.py

--- Info.py
+++ Info.py
@@ -6,7 +6,7 @@ twilionumber = ''
 twiliosmsnumber = ''
 
 #Telegram Bot Token
-API_TOKEN = "" 
+API_TOKEN = "8078682981:AAECiP6bH9D7N7T6pA0HnBjnuOfBHKyHIT0" 
 
 #Host URL
 callurl = 'https://03f3-196-191-60-243.ngrok.io' 
